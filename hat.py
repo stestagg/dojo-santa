d27255ce11e55c017be731d69651c49f61aa26e1
Matt Chung <itsmemattchung@gmail.com>
2015-12-03 20:44:51 +0000

    Added get_users to hat.py

--- hat.py
+++ hat.py
@@ -1,10 +1,22 @@
+import os
+import json
+
+def get_users():
+    current_directory = os.path.dirname(__file__)
+    users_db = os.path.join(current_directory, 'users.json')
+    with open(users_db) as fh:
+        return json.load(fh)
+
 
 def add(name):
-	pass
+    users = get_users()
+    pass
 
-def assign(name): name
-	# will return assigned partner
-	pass
+def assign(name):
+    pass
 
 def remove(name):
-	pass
+    pass
+
+if __name__ == "__main__":
+    print get_users()
